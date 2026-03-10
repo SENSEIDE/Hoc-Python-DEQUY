dc49c2ca7cdcebb58d74d8a45d2c23c01debcf01
YuDev <ngo86947@gmail.com>
2026-03-10 15:48:58 +0700

    Hàm Fibonacci Tối Ưu Hóa

--- DEQUY.py
+++ DEQUY.py
@@ -1,6 +1,7 @@
 def fibo(n):
-    if n <= 1:
-        return n
+    if n < 0: return "Không hợp lệ"
+    if n <= 1: return n
     return fibo(n-1) + fibo(n-2)
 
-print(fibo(5))
+n = 10
+print(f"Số Fibonacci thứ {n} là: {fibo(n)}")
